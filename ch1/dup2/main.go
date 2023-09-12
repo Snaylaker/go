04ad2ea0c9490141456d45d12b1905d79637dd04
Mehdi <mehdi@pop-os.localdomain>
2023-09-12 13:28:32 +0200

    chore(dup2): adding some logs

--- ch1/dup2/main.go
+++ ch1/dup2/main.go
@@ -13,6 +13,7 @@ func main() {
 		countLines(os.Stdin, counts)
 	} else {
 		for _, arg := range files {
+			fmt.Println("Processing:", arg)
 			f, err := os.Open(arg)
 			if err != nil {
 				fmt.Fprintf(os.Stderr, "dup2 : %v \n", err)
@@ -26,12 +27,14 @@ func main() {
 		if n > 1 {
 			fmt.Printf("%d\t%s\n", n, line)
 		}
+
 	}
 
 }
 
 func countLines(file *os.File, counts map[string]int) {
 	input := bufio.NewScanner(file)
+	fmt.Println("Testing if this is counting the lines")
 	for input.Scan() {
 		counts[input.Text()]++
 	}
